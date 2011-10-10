d875c2032e14203e23fd007ee9082ccf3c5bf3db
Alex Henning Johannessen <ahjohannessen@gmail.com>
2011-10-10 09:31:48 +0100

    Added missing files to Project. Removed debug related stuff as this can be done in FubuMVC itself for all transformers.

--- src/FubuMVC.Coffee/CoffeeTransformer.cs
+++ src/FubuMVC.Coffee/CoffeeTransformer.cs
@@ -1,28 +1,20 @@
 using System.Collections.Generic;
 using FubuMVC.Core.Assets.Content;
 using FubuMVC.Core.Assets.Files;
-using FubuMVC.Core.Diagnostics;
 
 namespace FubuMVC.Coffee
 {
     public class CoffeeTransformer : ITransformer
     {
         private readonly ICoffee _coffee;
-        private readonly IDebugDetector _debugDetector;
 
-        public CoffeeTransformer(ICoffee coffee, IDebugDetector debugDetector)
+        public CoffeeTransformer(ICoffee coffee)
         {
             _coffee = coffee;
-            _debugDetector = debugDetector;
         }
 
         public string Transform(string contents, IEnumerable<AssetFile> files)
         {
-            if(_debugDetector.IsDebugCall())
-            {
-                /* Log */      
-            }
-
             return _coffee.Compile(contents);
         }
     }
